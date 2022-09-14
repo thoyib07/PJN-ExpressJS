21014529392a77a40c15a72fe48159ccedba0ffd
Thoyib Hidayat <thoyib07@gmail.com>
2022-09-14 07:08:34 +0700

    unit test

---
+++ test/request.test.js
@@ -0,0 +1,13 @@
+import express from 'express';
+import request  from 'supertest';
+
+const app = express();
+
+app.get('/', (req,res) => {
+    res.send('Hello Thoyib!');
+});
+
+test("Test Expres", async () => {
+    const respon = await request(app).get('/');
+    expect(respon.text).toBe("Hello World!");
+});
