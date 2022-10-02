e89866cac2f2a075b078ef00dad7789c43c2afda
Thoyib Hidayat <thoyib07@gmail.com>
2022-10-02 16:02:48 +0700

    error handler

---
+++ test/error-handling.test.js
@@ -0,0 +1,21 @@
+import express from 'express';
+import request  from 'supertest';
+
+const app = express();
+
+const errorMiddle = (err, req, res, next) => {
+    res.status(500).send(`Terjadi Error: ${err.message}`);
+};
+
+app.get('/', (req,res) => {
+    // res.send(`Hello Response!`);
+    throw new Error("Ups salah nih");
+});
+app.use(errorMiddle);
+
+test("Test Response", async () => {
+    const respon = await request(app)
+        .get('/');
+    expect(respon.status).toBe(500);
+    expect(respon.text).toBe("Terjadi Error: Ups salah nih");
+});
